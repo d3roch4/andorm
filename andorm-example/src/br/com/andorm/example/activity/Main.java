621022eb260ae39a584a5c92183d523b6daa1eb4
Tiago Emerick <tiagoemerick@gmail.com>
2013-01-04 18:43:29 -0200

    teste

--- andorm-example/src/br/com/andorm/example/activity/Main.java
+++ andorm-example/src/br/com/andorm/example/activity/Main.java
@@ -11,5 +11,6 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // teste
     }
 }
